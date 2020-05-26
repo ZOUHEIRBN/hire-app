19285e4b93b54d1a9588a84abc5308250ead8a37
ZOUHEIRBN <zouheir.b97@gmail.com>
2020-05-26 12:28:58 +0100

    User conncetion notifications

--- src/app/notifications-list/notifications-list.component.ts
+++ src/app/notifications-list/notifications-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Injectable, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Injectable, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { SocketService } from '../services/socket.service';
 import { DataSource, CollectionViewer, SelectionChange } from '@angular/cdk/collections';
 import { BehaviorSubject, Observable, merge, Subscription } from 'rxjs';
@@ -8,8 +8,8 @@ import { Notification } from '../interfaces/notifications';
 
 
 /** Flat node with expandable and level information */
-export class DynamicFlatNode {
-  constructor(public item: string, public level = 1, public expandable = false,
+export class NotificationNode {
+  constructor(public title: string, public timestamp:Date, public type:string, public state=true, public level = 1, public expandable = false,
               public isLoading = false) {}
 }
 
@@ -22,8 +22,8 @@ export class DynamicDatabase {
   rootLevelNodes: string[] = ['Match', 'Watchout', 'User connections'];
 
   /** Initial data from database */
-  initialData(): DynamicFlatNode[] {
-    return this.rootLevelNodes.map(name => new DynamicFlatNode(name, 0, true));
+  initialData(): NotificationNode[] {
+    return this.rootLevelNodes.map(name => new NotificationNode(name, new Date(), name, true, 0, true));
   }
 
   getChildren(node: string): Notification[] | undefined {
@@ -35,24 +35,24 @@ export class DynamicDatabase {
   }
 }
 
-export class DynamicDataSource implements DataSource<DynamicFlatNode> {
+export class DynamicDataSource implements DataSource<NotificationNode> {
 
-  constructor(private _treeControl: FlatTreeControl<DynamicFlatNode>,
+  constructor(private _treeControl: FlatTreeControl<NotificationNode>,
     private _database: DynamicDatabase) {}
 
-  dataChange = new BehaviorSubject<DynamicFlatNode[]>([]);
+  dataChange = new BehaviorSubject<NotificationNode[]>([]);
 
-  get data(): DynamicFlatNode[] { return this.dataChange.value; }
-  set data(value: DynamicFlatNode[]) {
+  get data(): NotificationNode[] { return this.dataChange.value; }
+  set data(value: NotificationNode[]) {
     this._treeControl.dataNodes = value;
     this.dataChange.next(value);
   }
 
-  connect(collectionViewer: CollectionViewer): Observable<DynamicFlatNode[]> {
+  connect(collectionViewer: CollectionViewer): Observable<NotificationNode[]> {
     this._treeControl.expansionModel.changed.subscribe(change => {
-      if ((change as SelectionChange<DynamicFlatNode>).added ||
-        (change as SelectionChange<DynamicFlatNode>).removed) {
-        this.handleTreeControl(change as SelectionChange<DynamicFlatNode>);
+      if ((change as SelectionChange<NotificationNode>).added ||
+        (change as SelectionChange<NotificationNode>).removed) {
+        this.handleTreeControl(change as SelectionChange<NotificationNode>);
       }
     });
 
@@ -62,7 +62,7 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
   disconnect(collectionViewer: CollectionViewer): void {}
 
   /** Handle expand/collapse behaviors */
-  handleTreeControl(change: SelectionChange<DynamicFlatNode>) {
+  handleTreeControl(change: SelectionChange<NotificationNode>) {
     if (change.added) {
       change.added.forEach(node => this.toggleNode(node, true));
     }
@@ -71,11 +71,27 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
     }
   }
 
+  /** Add an item to to-do list */
+  insertItem(notification: Notification) {
+    let node = new NotificationNode(notification.title, <Date>notification.timestamp, notification.type)
+    this.dataChange.next(this.data);
+  }
+
+  updateItem(node: NotificationNode, notification: Notification) {
+    node.state = notification.state;
+    node.type = notification.type
+    node.title = notification.title
+    this.dataChange.next(this.data);
+  }
+  updateItemState(node: NotificationNode, state: boolean) {
+    node.state = state;
+    this.dataChange.next(this.data);
+  }
   /**
    * Toggle the node, remove from display list
    */
-  toggleNode(node: DynamicFlatNode, expand: boolean) {
-    const children = this._database.getChildren(node.item);
+  toggleNode(node: NotificationNode, expand: boolean) {
+    const children = this._database.getChildren(node.title);
     const index = this.data.indexOf(node);
     if (!children || index < 0) { // If no children, or cannot find the node, no op
       return;
@@ -85,10 +101,11 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
 
     setTimeout(() => {
       if (expand) {
-        const nodes = children.map(name =>
-          new DynamicFlatNode(name.title, node.level + 1, this._database.isExpandable(name.title)));
+        const nodes = children.map(notification =>
+          new NotificationNode(notification.title, <Date>notification.timestamp, notification.type, node.state, node.level + 1, this._database.isExpandable(notification.title)));
         this.data.splice(index + 1, 0, ...nodes);
-      } else {
+      }
+      else {
         let count = 0;
         for (let i = index + 1; i < this.data.length
           && this.data[i].level > node.level; i++, count++) {}
@@ -98,7 +115,7 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
       // notify the change
       this.dataChange.next(this.data);
       node.isLoading = false;
-    }, 10);
+    }, 1500);
   }
 }
 
@@ -109,41 +126,62 @@ export class DynamicDataSource implements DataSource<DynamicFlatNode> {
   templateUrl: './notifications-list.component.html',
   styleUrls: ['./notifications-list.component.css']
 })
-export class NotificationsListComponent {
+export class NotificationsListComponent implements AfterViewInit {
   @Output() countUpdate = new EventEmitter()
   constructor(public database: DynamicDatabase, private  _socketService:SocketService) {
-    this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
+    this.treeControl = new FlatTreeControl<NotificationNode>(this.getLevel, this.isExpandable);
     this.dataSource = new DynamicDataSource(this.treeControl, database);
-
     this.dataSource.data = database.initialData();
+  }
 
+  ngAfterViewInit(){
     this._socketService.onNotification().subscribe(res => {
-      if(res.type.toString().toLowerCase() == "user_connection"){
-        console.log(this.database.dataMap)
-        let arr = this.database.dataMap.get('User connections')
-        arr.push(res)
-        this.database.dataMap.set('User connections', arr)
+      if(['wanted', 'match'].includes(res.type.toString().toLowerCase())){
+        //Make a sound
       }
 
-      let keys = Array.from( this.database.dataMap.keys() );
-      let total_notifications = 0
-      for(let k of keys){
-        total_notifications += this.database.dataMap.get(k).length
-      }
-      this.countUpdate.emit(total_notifications)
+      //Add node to notifications
+      let arr = this.database.dataMap.get(res.type.toString())
+      arr.push(res)
+      this.database.dataMap.set(res.type.toString(), arr)
+      this.refreshListCounts()
     })
-
 
   }
 
-  treeControl: FlatTreeControl<DynamicFlatNode>;
+  refreshListCounts(){
+    let keys = Array.from( this.database.dataMap.keys() );
+    let total_notifications = 0
+    for(let k of keys){
+      total_notifications += this.database.dataMap.get(k).filter(x => x.state == true).length
+    }
+    this.countUpdate.emit(total_notifications)
+  }
+
+  markAsRead(node: NotificationNode){
+    this.dataSource.updateItemState(node!, false);
+    this.refreshListCounts()
+  }
+
+  addNewItem(node: NotificationNode) {
+    const parentNode = this.database.dataMap.get(node.type);
+    this.dataSource.insertItem(node);
+    this.treeControl.expand(node);
+  }
+  saveNode(node: NotificationNode) {
+    const nestedNode = this.database.dataMap.get(node.type);
+    console.log(nestedNode)
+    this.dataSource.updateItem(node!, new Notification('Title', 'User connection'));
+  }
+
+  treeControl: FlatTreeControl<NotificationNode>;
 
   dataSource: DynamicDataSource;
 
-  getLevel = (node: DynamicFlatNode) => node.level;
+  getLevel = (node: NotificationNode) => node.level;
 
-  isExpandable = (node: DynamicFlatNode) => node.expandable;
+  isExpandable = (node: NotificationNode) => node.expandable;
 
-  hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
+  hasChild = (_: number, _nodeData: NotificationNode) => _nodeData.expandable;
 
 }
